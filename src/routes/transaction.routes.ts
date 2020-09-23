fb8daef429c842fde9d6652ccbec9866620bd61d
bmodepri <garcia.priscila01@gmail.com>
2020-09-23 14:16:29 +0200

    complete challenge

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -2,9 +2,6 @@ import { Router } from 'express';
 import CreateTransactionService from '../services/CreateTransactionService';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-
 const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
